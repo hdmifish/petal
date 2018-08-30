d57edb2a51a6c5508b16f3f208e9270615f1373f
Davarice <jsdreim@gmail.com>
2018-08-29 21:25:27 -0400

    Prepared for integration with commands.py
    Fixed idiocy

--- petal/mcname.py
+++ petal/mcname.py
@@ -2,7 +2,6 @@ import json
 import requests
 
 dbName = "playerdb.json" # file in which userdata is stored
-
 """
 ERROR CODES:
  0: Successful cmmnd: user added to local database to be approved and synced with whitelist
@@ -16,7 +15,6 @@ ERROR CODES:
 -8: Malevolent error: user supplied erroneous name
 -9: Malevolent error: incomplete function (fault of developer)
 """
-
 def breakUID(str0): # Break apart Mojang UUID with dashes
     str1 = str0[0:8]
     str2 = str0[8:12]
@@ -27,7 +25,6 @@ def breakUID(str0): # Break apart Mojang UUID with dashes
     return str99
 
 def writeLocalDB(player, dbIn): # update db from ephemeral player; write db to file
-    
     pIndex = next((item for item in dbIn if item["uuid"] == player["uid_mc"]), False) # DBase index of player (integer 0+)
     ret = -2
     if pIndex == False: # Player is not in the database -- Create entry
@@ -43,10 +40,6 @@ def writeLocalDB(player, dbIn): # update db from ephemeral player; write db to f
     json.dump(dbIn, open(dbName, 'w'), indent=4)
     return ret
 
-def createLocalDB(player0): # use the ephemeral player as the first entry in a new file
-    
-    return -9
-
 def addToLocalDB(userdat, submitter): # Add UID and username to local whitelist database
     uid = userdat["id"]
     uidF = breakUID(uid)
@@ -59,20 +52,26 @@ def addToLocalDB(userdat, submitter): # Add UID and username to local whitelist 
         }
     try:
         dbRead = json.load(open(dbName)) # dbRead is now a python object
-    except OSError: # TODO: file does not exist: create the file
+    except OSError: # File does not exist: Create the file
         dbRead = [{'uuid': uidF, 'name': [uname]}]
-    writeLocalDB(eph, dbRead)
+    return writeLocalDB(eph, dbRead)
 
 def idFromName(uname_raw):
     uname_low = uname_raw.lower()
     response = requests.get("https://api.mojang.com/users/profiles/minecraft/{}".format(uname_low))
-    return {'code':response.status_code, 'udat':response.json() }
+    if response.status_code == 204:
+        return {'code':response.status_code}
+    else:
+        return {'code':response.status_code, 'udat':response.json() }
 
 def WLRequest(nameGiven, discord_id):
     udict = idFromName(nameGiven) # Get the id from the name, or an error
     if udict["code"] == 200: # If this is 200, the second part will contain json data; Try to add it
         verdict = addToLocalDB(udict["udat"], discord_id)
+        return verdict
 # Map response codes to function errors
+    elif udict["code"] == 204:
+        return -8
     #elif udict["code"] == 200:
         #return 
     else:
