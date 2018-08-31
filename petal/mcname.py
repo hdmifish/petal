82b9ea6c5e5850c34d3af8a2282345cb8b04a4d7
Davarice <jsdreim@gmail.com>
2018-08-31 16:41:22 -0400

    Major optimizations to nearly all functions

--- petal/mcname.py
+++ petal/mcname.py
@@ -22,7 +22,7 @@ ERROR CODES:
     # (Do not use this)
 PLAYERDEFAULT = OrderedDict([('name', 'PLAYERNAME'), ('uuid', '00000000-0000-0000-0000-000000000000'), ('altname', []), ('discord', '000000000000000000'), ('approved', []), ('submitted', '1970-01-01_00:00'), ('suspended', False)])
 
-def EXPORT_WHITELIST(refreshall=False):
+def EXPORT_WHITELIST(refreshall=False, refreshnet=False):
     # Export the local database into the whitelist file itself
     # If Mojang ever changes the format of the server whitelist file, this is the function that will need to be updated
     try:
@@ -63,73 +63,84 @@ def breakUID(str0): # Break apart Mojang UUID with dashes
     str99 = str1 + "-" + str2 + "-" + str3 + "-" + str4 + "-" + str5
     return str99
 
-def writeLocalDB(player, dbIn): # update db from ephemeral player; write db to file
-    pIndex = next((item for item in dbIn if item["uuid"] == player["uid_mc"]), False) # Is the player found in the list?
-    ret = -2
+def writeLocalDB(player): # update db from ephemeral player; write db to file
 
-    if pIndex == False: # Player is not in the database -- Create entry
-        pIndex = len(dbIn) # Where the new player is about to be
-        dbIn.append({})
-        dbIn[pIndex] = {'uuid': player["uid_mc"], 'name': player["uname"], 'altname': [], 'discord': player["uid_dis"], 'approved': player["approved"], 'suspended': player["suspended"]}
-        dbIn[pIndex]["submitted"] = datetime.datetime.today().strftime('%Y-%m-%d_%0H:%M')
-        ret = 0
-    else:
-        pIndex = dbIn.index(pIndex) # DBase index of player (integer 0+)
-        if len(dbIn[pIndex]["approved"]) > 0: # If the user is approved, change feedback
-            ret = -1
-
-    # Fetch username history
-    dbIn[pIndex]["altname"] = []
-    namehist = requests.get("https://api.mojang.com/user/profiles/{}/names".format(player["uid_mc"].replace("-","")))
-
-    if namehist.status_code == 200:
-        for name in namehist.json():
-            dbIn[pIndex]["altname"].append(name["name"])
-
-
-    dbIn[pIndex]["discord"] = player["uid_dis"]
-    json.dump(dbIn, open(dbName, 'w'), indent=2)
-
-    return ret
-
-def addToLocalDB(userdat, submitter): # Add UID and username to local whitelist database
-    uid = userdat["id"]
-    uidF = breakUID(uid)
-    uname = userdat["name"]
-    eph = { # Create dict: Ephemeral player profile, to be merged into dbRead
-        "uname" : uname, # Minecraft username; append to dbase usernames
-        "uid_mc" : uidF, # Minecraft UID; use to locate or create dbase entry
-        "uid_dis" : submitter, # Discord UID; attach to mc uid if not present
-        "approved" : [],
-        "suspended" : False
-        }
     try:
         dbRead = json.load(open(dbName), object_pairs_hook=OrderedDict) # dbRead is now a python object
     except OSError: # File does not exist: Create the file
         dbRead = [{'uuid': uidF, 'name': [uname]}]
-    return writeLocalDB(eph, dbRead), uidF
 
+    pIndex = next((item for item in dbRead if item["uuid"] == player["uuid"]), False) # Is the player found in the list?
+
+    if pIndex == False: # Player is not in the database -- Create entry
+
+        # Fetch username history
+        namehist = requests.get("https://api.mojang.com/user/profiles/{}/names".format(player["uuid"].replace("-","")))
+        if namehist.status_code == 200:
+            player["altname"] = []
+            for name in namehist.json():
+                player["altname"].append(name["name"])
+
+        dbRead.append(player) # Set up a new profile with all the right fields
+        ret = 0
+    else:
+        pIndex = dbRead.index(pIndex) # DBase index of player (integer 0+)
+        if len(dbRead[pIndex]["approved"]) > 0: # If the user is approved, say something different
+            ret = -1
+        else:
+            ret = -2
+    try:
+        json.dump(dbRead, open(dbName, 'w'), indent=2) # Save all the things
+    except OSError: # Cannot write file: Well this was all rather pointless
+        ret = -7
+    return ret
+
+# User wants to be whitelisted? Add to the database for approval
+def addToLocalDB(userdat, submitter):
+    uid = userdat["id"]
+    uidF = breakUID(uid)
+    uname = userdat["name"]
+    eph = { # Create dict: Ephemeral player profile, to be merged into dbRead
+        "name" : uname, # Minecraft username; append to dbase usernames
+        "uuid" : uidF, # Minecraft UID; use to locate or create dbase entry
+        "discord" : submitter, # Discord UID; attach to mc uid if not present
+        "submitted" : datetime.datetime.today().strftime('%Y-%m-%d_%0H:%M')
+        }
+    # Apply the values to a blank slate
+    pNew = PLAYERDEFAULT.copy() # Get the slate
+    pNew.update(eph) # Imprint anything new from the player
+    return writeLocalDB(pNew), uidF
+
+# User gave us a username? Text is worthless. Hey Mojang, UUID is this name?
 def idFromName(uname_raw):
     uname_low = uname_raw.lower()
     response = requests.get("https://api.mojang.com/users/profiles/minecraft/{}".format(uname_low))
-    if response.status_code == 204:
-        return {'code':response.status_code}
-    else:
+    if response.status_code == 200:
         return {'code':response.status_code, 'udat':response.json() }
+    else:
+        return {'code':response.status_code}
 
+###---
+##  Top Level Commands - Invoked by Discord users
+###---
+
+# !wlme <username>
 def WLRequest(nameGiven, discord_id):
     udict = idFromName(nameGiven) # Get the id from the name, or an error
     if udict["code"] == 200: # If this is 200, the second part will contain json data; Try to add it
         verdict, uid = addToLocalDB(udict["udat"], discord_id)
         return verdict, uid
-# Map response codes to function errors
+    # Map response codes to function errors
     elif udict["code"] == 204:
         return -8, "x"
     #elif udict["code"] == 200:
         #return 
     else:
-        return "Nondescript Error ({})".format(udict["code"])
+        return "Nondescript API Error ({})".format(udict["code"])
 
+
+
+# !wl <ticket>
 def WLAdd(idTarget, idSponsor):
     try:
         dbRead = json.load(open(dbName), object_pairs_hook=OrderedDict) # dbRead is now a python object
@@ -165,6 +176,9 @@ def WLAdd(idTarget, idSponsor):
     json.dump(dbRead, open(dbName, 'w'), indent=2)
     return ret, doSend, targetid, targetname, EXPORT_WHITELIST()
 
+
+
+# !wlquery <ticket>
 def WLQuery(instr):
     try:
         dbRead = json.load(open(dbName)) # dbRead is now a python object
