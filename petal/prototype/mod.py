464120f5ae469162ed3f69cbfafab01a55bf531b
Davarice <jsdreim@gmail.com>
2018-10-29 17:27:09 -0400

    Real Commands modules with core.Commands subclassed
    These are the modules where all the command methods will really go

---
+++ petal/prototype/mod.py
@@ -0,0 +1,8 @@
+from . import core
+
+class CommandsMod(core.Commands):
+    pass
+
+# Keep the actual classname unique from this common identifier
+# Might make debugging nicer
+CommandModule = CommandsMod
---
+++ petal/prototype/util.py
@@ -0,0 +1,8 @@
+from . import core
+
+class CommandsUtil(core.Commands):
+    pass
+
+# Keep the actual classname unique from this common identifier
+# Might make debugging nicer
+CommandModule = CommandsUtil
