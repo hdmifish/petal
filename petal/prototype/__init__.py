8299ddbb3cff5d5dfa055a299213dbb4041776f1
Davarice <jsdreim@gmail.com>
2018-10-29 17:05:08 -0400

    Finding command method separated from execution of it
    This will allow the help command to use the same code to find the docstring

--- petal/prototype/__init__.py
+++ petal/prototype/__init__.py
@@ -22,23 +22,34 @@ class CommandRouter:
             except:
                 pass
 
+    def find_command(self, kword):
+        """
+        Find and return a class method whose name matches kword
+        """
+        func = None
+        for mod in self.commands:
+            func = mod.get_command(kword)
+            if not func:
+                continue
+            else:
+                break
+        return func
+
     def route(self, command: str, src=None):
         """
         Route a command (and the source message) to the correct method of the correct module.
         By this point, the prefix should have been stripped away already, leaving a plaintext command.
         """
+        # 'ban badperson666 evilness'
         command_components = command.split(
             " ", 1
         )  # Separate the first word from the rest
+        # ['ban', 'badperson666 evilness']
         command_word = command_components.pop(0)
-        func = None
-        for mod in self.commands:
-            func = mod.get_command(command_word)
-            if not func:
-                continue
-            else:
-                break
+        # 'ban'; ['badperson666 evilness']
+
+        func = self.find_command(command_word)  # Find the method
         if not func:
             return "Command '{}' not found.".format(command_word)
         else:
-            return func(command_components, src)
+            return func(command_components, src)  # Execute it
