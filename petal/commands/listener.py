f00417799f70dbe83a914d61c7da7c0fc47d9257
Davarice <jsdreim@gmail.com>
2019-03-09 19:35:08 -0500

    Listener Authentication function, mostly cloned from Moderation module

--- petal/commands/listener.py
+++ petal/commands/listener.py
@@ -1,12 +1,22 @@
 """Commands module for LISTENER-RELATED UTILITIES.
 Access: Role-based"""
 
+import discord
+
 from . import core
 
 
 class CommandsListener(core.Commands):
-    def authenticate(self, *_):
-        return False
+    def authenticate(self, src):
+        target = discord.utils.get(src.author.server.roles, name="Listener")
+        if target is None:
+            self.log.err("Listener role does not exist")
+            return False
+        else:
+            if target in src.author.roles:
+                return True
+            else:
+                return False
 
 
 # Keep the actual classname unique from this common identifier
