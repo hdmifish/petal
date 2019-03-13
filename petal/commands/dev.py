16de0046662222681d2679347eab0eb80e7ff2ae
Davarice <jsdreim@gmail.com>
2019-03-13 16:00:09 -0400

    Import and adapt !blacklist command

--- petal/commands/dev.py
+++ petal/commands/dev.py
@@ -25,6 +25,29 @@ class CommandsMaintenance(core.Commands):
         """
         return "Hello boss! How's it going?"
 
+    async def cmd_blacklist(self, args, src, **_):
+        """
+        Prevent user of given ID(s) from using Petal.
+
+        The user will not be able to access any features of Petal, but will still be logged in the member database. Multiple IDs may be provided, space-separated.
+
+        Syntax: `{p}blacklist <user_ID> [<user_ID> [<user_ID> [...]]]`
+        """
+        if not args:
+            return "Provide at least one User ID."
+        else:
+            for uid in args:
+                mem = self.get_member(src, uid)
+                if mem is None:
+                    return "Couldnt find user with ID: " + uid
+
+                if mem.id in self.config.blacklist:
+                    self.config.blacklist.remove(mem.id)
+                    return mem.name + " was removed from the blacklist."
+                else:
+                    self.config.blacklist.append(mem.id)
+                    return mem.name + " was blacklisted."
+
 
 # Keep the actual classname unique from this common identifier
 # Might make debugging nicer
