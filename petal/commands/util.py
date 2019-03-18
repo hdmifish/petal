defa0ea9a63be594582aea7b45f458deb2046546
Davarice <jsdreim@gmail.com>
2019-03-17 21:41:20 -0400

    Add "exhaustive help" invoked with '!help extreme'

--- petal/commands/util.py
+++ petal/commands/util.py
@@ -8,6 +8,15 @@ import discord
 from petal.commands import core
 
 
+helptext = [
+    """An __Argument__ is simply any word given to a command. Arguments are separated from each other by spaces.```{p}command asdf qwert zxcv```Running this command would pass three Arguments to the command: `"asdf"`, `"qwert"`, and `"zxcv"`. It is up to the command function to decide what Arguments it wants, and how they are used.""",
+    """While spaces separate Arguments, sometimes an Argument is desired to be multiple words. In these cases, one can simply enclose the argument in quotes; For example:```{p}command "asdf qwert" zxcv```This would pass only *two* arguments to the command: `"asdf qwert"` and `"zxcv"`.""",
+    """An __Option__ is an additional Argument passed to a command, prefixed by either `-` or `--`, which is optional. An Option prefixed by `-` is a "Short" Option, while an Option prefixed by `--` is a "Long" Option. For example:```{p}command --opt=asdf```If you were to run this, you would pass the Long Option, `opt`, into the command with the value of `"asdf"`. It is up to the `cmd_command()` method to accept and use this value somehow.""",
+    """A __Flag__ is an Option passed without an explicit value, such as in:```{p}command --verbose```In this example, `verbose` is passed into the command with a **boolean** value of `True`, rather than any string value. This is often used by commands that may optionally return more information if requested.""",
+    """Short Options may be grouped together as a single prefixed word, or cluster. This can save time when typing a command with a series of Flags, but it is less useful when values need to be passed, because only the final Short Option in a cluster will be assigned the value specified. For example:```{p}command -abc=23 --long1=xyz --long2```In this command, while `c` is passed with a value of `"23"`, `a` and `b` are simply passed with values of `True`. This is the same difference by which the Long Option `long1` is passed with the value `"xyz"` while `long2` is passed with the value `True`.""",
+]
+
+
 class CommandsUtil(core.Commands):
     auth_fail = "This command is public. If you are reading this, something went wrong."
 
@@ -15,19 +24,28 @@ class CommandsUtil(core.Commands):
         return True
 
     async def cmd_help(self, args, src, **_):
-        """
-        Print information regarding command usage.
+        """Print information regarding command usage.
 
-        Help text is drawn from the docstring of a command method, which should be formatted into three sections -- Summary, Details, and Syntax -- which are separated by double-newlines.
-        The Summary section provides cursory information about a command, and is typically all one needs to understand it.
-        The Details section contains more involved information about how the command works, possibly including technical information.
-        The Syntax section describes exactly how the command should be invoked. Angle brackets indicate a parameter to be filled, square brackets indicate an optional segment, and parentheses indicate choices, separated by pipes.
+        Help text is drawn from the docstring of a command method, which should be formatted into four sections -- Summary, Details, Syntax, and Options -- which are separated by double-newlines.
+        The __Summary__ section provides cursory information about a command, and is typically all one needs to understand it.
+        The __Details__ section contains more involved information about how the command works, possibly including technical information.
+        The __Syntax__ section describes exactly how the command should be invoked. Angle brackets indicate a parameter to be filled, square brackets indicate an optional segment, and parentheses indicate choices, separated by pipes.
+        The __Options__ section details Options and Flags that may be passed to the command. These may significantly alter the operation of a command.
 
-        Syntax: `{p}help [<command>]`
+        For exhaustive help with Arguments and Options, invoke `{p}help extreme`.
+
+        Syntax: `{p}help [(<command>|extreme)]`
         """
         if not args:
             # TODO: Iso, put your default helptext here; Didnt copy it over in case you wanted it changed
             return "`<Default helptext goes here>`\n`#BlameIso`"
+
+        if args[0].lower() == "extreme":
+            for line in helptext:
+                await self.client.send_message(
+                    src.author, src.channel, line.format(p=self.config.prefix)
+                )
+            return
 
         mod, cmd, denied = self.router.find_command(args[0], src)
         if denied:
@@ -110,9 +128,7 @@ class CommandsUtil(core.Commands):
         return line_1 + line_2 + ": ```" + formattedList + "```"
 
     async def cmd_ping(self, src, **_):
-        """
-        Show the round trip time from this bot to Discord (not you) and back.
-        """
+        """Show the round trip time from this bot to Discord (not you) and back."""
         msg = await self.client.send_message(src.author, src.channel, "*hugs*")
         delta = int((dt.now() - msg.timestamp).microseconds / 1000)
         self.config.stats["pingScore"] += delta
@@ -126,9 +142,7 @@ class CommandsUtil(core.Commands):
         )
 
     async def cmd_statsfornerds(self, src, **_):
-        """
-        Display more detailed statistics (for nerds).
-        """
+        """Display more detailed statistics (for nerds)."""
         truedelta = int(self.config.stats["pingScore"] / self.config.stats["pingCount"])
 
         em = discord.Embed(title="Stats", description="*for nerds*", colour=0x0ACDFF)
@@ -160,7 +174,11 @@ class CommandsUtil(core.Commands):
     async def cmd_argtest(self, args, src, **opts):
         """Display details on how the command was parsed.
 
-        Used for testing, or personal experimentation to help you to understand options and flags.
+        Used for testing, or personal experimentation to help you to understand Arguments, Options and Flags.
+
+        When a command is run, all text typed after the command is sent to the command as a series of words.
+
+        An __Argument__ is simply any word given to a command. Arguments are separated from each other by spaces.```{p}command asdf qwert zxcv```Running this command would pass three Arguments to the command: `"asdf"`, `"qwert"`, and `"zxcv"`. It is up to the command function to decide what Arguments it wants, and how they are used.
 
         Syntax: `{p}argtest [-<abcd...>=<value>] [--<flag>=<value>]`
         """
