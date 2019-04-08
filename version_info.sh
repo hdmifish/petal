0cd4ba50404499bbce678799c114263142b59ef4
Davarice <jsdreim@gmail.com>
2019-04-07 21:14:47 -0400

    Update changelogs

--- version_info.sh
+++ version_info.sh
@@ -3,11 +3,11 @@
 # Not necessarily the prettiest but easy to work with.
 
 # MATCH THIS TO THE VERSION IN PETAL
-VERSION=0.7.0
+VERSION=0.7.1
 
 #(optional) what the webhook message should say.
 UPDATE_TITLE="Preparing to Prepare"
 
 # Separate bullet points with hyphens and use \n characters please.
-CHANGELOG="A primarily administrative update, and maybe some features\n+ Filled out changelog.txt\n*!statsfornerds to !stats\n*!list_connected_servers to !servers\n+ !define command\n+ !info command\n+ Added rudimentary type-checking for --options"
+CHANGELOG="* Fixed a mistake where !wlme would look for a config option as an attribute of Config, rather than going through Config.get()\n* Fixed an oversight wherein a failure to send a notification DM would not be correctly reported\n+ Added flag --nosend to !wlaccept to prevent sending DM notification"
 
