34fee5cb73d72743e42c8448ef35edb314354de8
Davarice <jsdreim@gmail.com>
2019-05-17 23:52:27 -0400

    Flesh out Exception docstrings

--- petal/exceptions.py
+++ petal/exceptions.py
@@ -2,22 +2,44 @@
 
 
 class CommandArgsError(Exception):
-    """Raised when a Command is invoked with malformed arguments."""
+    """Raised when a Command is invoked with malformed arguments.
+        Raising this Exception will allow the user to rerun the command by
+        editing their message. Therefore it should NOT be raised if any concrete
+        changes have been made by the Command already.
+    """
     pass
 
 
 class CommandAuthError(Exception):
-    """Raised when a Command is invoked with insufficient permissions."""
+    """Raised when a Command is invoked with insufficient permissions.
+        Raising this Exception will NOT allow the user to rerun the command by
+        editing. It should be safe to raise no matter how far in the Command is.
+    """
+    pass
+
+
+class CommandExit(Exception):
+    """Raised when a Command wants to stop IMMEDIATELY, for whatever reason.
+        Raising this Exception will NOT allow the user to rerun the command by
+        editing. It should be safe to raise no matter how far in the Command is.
+    """
     pass
 
 
 class CommandInputError(Exception):
-    """Raised when a Command is given bad input."""
+    """Raised when a Command is given bad input.
+        Raising this Exception will allow the user to rerun the command by
+        editing their message. Therefore it should NOT be raised if any concrete
+        changes have been made by the Command already.
+    """
     pass
 
 
 class CommandOperationError(Exception):
-    """Raised when a Command fails, but NOT due to bad input."""
+    """Raised when a Command fails, but NOT due to bad input.
+        Raising this Exception will NOT allow the user to rerun the command by
+        editing. It should be safe to raise no matter how far in the Command is.
+    """
     pass
 
 
