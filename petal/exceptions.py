98df4604ed337ea923eca42e15ab3e801990a524
Davarice <jsdreim@gmail.com>
2019-07-21 00:21:31 -0400

    Implement proper Exception Hierarchy

--- petal/exceptions.py
+++ petal/exceptions.py
@@ -1,7 +1,22 @@
-"""Petal Exception module"""
+"""Petal Exceptions module.
+
+Implements a hierarchy of Petal Exceptions. At the top is the PetalError. Below
+    the PetalError are Exceptions regarding general functions, such as Commands.
+    The tier below that is for specific problems that arise.
+"""
 
 
-class CommandArgsError(Exception):
+class PetalError(Exception):
+    """Superclass for all Exceptions related to Petal."""
+    pass
+
+
+class CommandError(PetalError):
+    """Superclass for Exceptions regarding Commands."""
+    pass
+
+
+class CommandArgsError(CommandError):
     """Raised when a Command is invoked with malformed arguments.
         Raising this Exception will allow the user to rerun the command by
         editing their message. Therefore it should NOT be raised if any concrete
@@ -10,7 +25,7 @@ class CommandArgsError(Exception):
     pass
 
 
-class CommandAuthError(Exception):
+class CommandAuthError(CommandError):
     """Raised when a Command is invoked with insufficient permissions.
         Raising this Exception will NOT allow the user to rerun the command by
         editing. It should be safe to raise no matter how far in the Command is.
@@ -18,7 +33,7 @@ class CommandAuthError(Exception):
     pass
 
 
-class CommandExit(Exception):
+class CommandExit(CommandError):
     """Raised when a Command wants to stop IMMEDIATELY, for whatever reason.
         Raising this Exception will NOT allow the user to rerun the command by
         editing. It should be safe to raise no matter how far in the Command is.
@@ -26,7 +41,7 @@ class CommandExit(Exception):
     pass
 
 
-class CommandInputError(Exception):
+class CommandInputError(CommandError):
     """Raised when a Command is given bad input.
         Raising this Exception will allow the user to rerun the command by
         editing their message. Therefore it should NOT be raised if any concrete
@@ -35,7 +50,7 @@ class CommandInputError(Exception):
     pass
 
 
-class CommandOperationError(Exception):
+class CommandOperationError(CommandError):
     """Raised when a Command fails, but NOT due to bad input.
         Raising this Exception will NOT allow the user to rerun the command by
         editing. It should be safe to raise no matter how far in the Command is.
@@ -43,7 +58,12 @@ class CommandOperationError(Exception):
     pass
 
 
-class TunnelHobbled(Exception):
+class TunnelError(PetalError):
+    """Superclass for Exceptions regarding Tunnels."""
+    pass
+
+
+class TunnelHobbled(TunnelError):
     """Raised when a Tunnel is unable to comply with an order. Should always
         be passed details about what the Tunnel cannot do.
     """
@@ -53,6 +73,6 @@ class TunnelHobbled(Exception):
     pass
 
 
-class TunnelSetupError(Exception):
+class TunnelSetupError(TunnelError):
     """Raised when a Tunnel fails to be established."""
     pass
