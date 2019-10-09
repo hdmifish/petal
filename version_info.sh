df92edffb56c3deb3b96d1ab64abcabfb23ac5ce
Davarice <jsdreim@gmail.com>
2019-10-08 23:47:14 -0400

    Update Changelog

--- version_info.sh
+++ version_info.sh
@@ -3,7 +3,7 @@
 # Not necessarily the prettiest but easy to work with.
 
 # Petal reads this line.
-VERSION="1.0.0-rc2"
+VERSION="1.0.0-rc3"
 
 #(optional) what the webhook message should say.
 UPDATE_TITLE="The Great Migration"
@@ -11,17 +11,22 @@ UPDATE_TITLE="The Great Migration"
 CHANGELOG=$(cat <<'EOF'
 * Migrated to version 1.x of Discord Library.
 * All IDs now Integers.
-* Config.get() now allows "pathed" option fetching.
+* Config.get() now allows "pathed" option fetching, and can be called by Subscripting.
 * Servers are now called "Guilds".
+* Revised Join/Part Messages.
+* Wrapped periodic Coroutines in a Task that reports when they fail.
 + Added several Petal Exceptions for various situations, allowing Commands to fail explicitly.
++ Implemented Color Enumeration to facilitate standard colors in Embeds.
 + Implemented "saved" commands, allowing much greater flexibility in re-running using Exceptions.
 + Implemented module standardizing the process of waiting for user responses.
 + Implemented module standardizing creation of check predicates for passing to Discord Library.
 + Additional Command returns: Dict, unpacked directly into send(); Embed, posted directly; Yield, iterated and sequentially appended to output before final return.
 + Added `--set` option to !osu, replacing functionality of !setosu.
++ Implemented !bytes command, for debugging the EXACT contents of a String.
 + Implemented !plane command, for forcing your problems and worries onto anyone environmentally conscious and unfortunate enough to pick up the wrong piece of abandoned paper.
 + Implemented !send command, for posting embeds to other channels.
 + Implemented Channel Tunneling with !tunnel, allowing Petal to act as a bridge between Messageables; Potential for use in future remake of !anon.
++ Implemented !userinfo command, for displaying.....user info.
 + Implemented !when command, for time zone conversions.
 + Implemented utility functions for generating Ordinals and wording numbers.
 - Removed !setosu.
