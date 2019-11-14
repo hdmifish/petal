1ca59027b442f10fef1a66831a91cade6f732b8e
Davarice <jsdreim@gmail.com>
2019-11-14 00:02:07 -0500

    Implement Activity Info

--- petal/util/embeds.py
+++ petal/util/embeds.py
@@ -2,12 +2,13 @@
 
 from datetime import datetime as dt, timedelta as td
 from enum import IntEnum
-from typing import Union
+from typing import Optional, Tuple, Union
 
-from discord import Embed, Guild, Member, User
+from discord import Activity, Embed, Game, Guild, Member, Spotify, Streaming, User
 
 from .cdn import get_avatar
 from .fmt import bold, escape, smallid
+from .grammar import sequence_words
 
 
 Muser = Union[Member, User]
@@ -25,6 +26,40 @@ class Color(IntEnum):
     xkcd = 0x_96_A8_C8  # XKCD
 
 
+def simple_activity(member: Muser) -> Tuple[str, Optional[str]]:
+    a = member.activity
+
+    if not isinstance(a, Activity):
+        return "Activity", "None"
+
+    elif isinstance(a, Game) or a.to_dict().get("type", -1) == 0:
+        return "In-Game", escape(a.name)
+
+    elif isinstance(a, Streaming):
+        return (
+            "Streaming",
+            "{}\n{}\n\n{}".format(
+                escape(repr(a.twitch_name or a.name)), a.details, a.url
+            ),
+        )
+
+    elif isinstance(a, Spotify):
+        return (
+            "Spotify",
+            "{}\nby {}\non {}".format(
+                escape(repr(a.title)),
+                sequence_words([escape(repr(x)) for x in a.artists]),
+                escape(repr(a.album)),
+            ),
+        )
+
+    else:
+        try:
+            return a.name, escape(a.details or a.state or "None")
+        except:
+            return "Activity: UNKNOWN", f"```python\n{a.to_dict()!r}```"
+
+
 def membership_card(member: Muser, *, colour: int = None) -> Embed:
     """Create an Embed Object showing Member/User Details."""
     created_at: dt = member.created_at.replace(microsecond=0)
@@ -35,6 +70,7 @@ def membership_card(member: Muser, *, colour: int = None) -> Embed:
     since_joined: td = now - joined_at
 
     guild: Guild = member.guild
+    act_name, act_desc = simple_activity(member)
 
     em = (
         Embed(
@@ -42,16 +78,19 @@ def membership_card(member: Muser, *, colour: int = None) -> Embed:
             description=f"Member of {bold(guild.name)}"
             f"\n{member.mention}"
             f"\n{escape(ascii(member.display_name))}"
-            f"\n`[{smallid(member.id)}]`",
+            f"\n`{smallid(member.id)}`",
             colour=member.colour if colour is None else colour,
-            timestamp=now,
+            # timestamp=now,
         )
         .set_thumbnail(url=get_avatar(member))
         .set_footer(text=f"{member.name}#{member.discriminator} / {member.id}")
+        .add_field(name=act_name, value=act_desc, inline=False)
+        .add_field(
+            name="Account Created", value=f"{created_at}\n({bold(since_created)} ago)"
+        )
+        .add_field(
+            name="Joined Guild", value=f"{joined_at}\n({bold(since_joined)} ago)"
+        )
     )
-    em.add_field(
-        name="Account Created", value=f"{created_at}\n({bold(since_created)} ago)"
-    )
-    em.add_field(name="Joined Guild", value=f"{joined_at}\n({bold(since_joined)} ago)")
 
     return em
