52df20ce84aa938d67fdaa25bc2eeb667334937a
Davarice <jsdreim@gmail.com>
2019-12-04 17:19:04 -0500

    Update changelogs

--- version_info.sh
+++ version_info.sh
@@ -3,7 +3,7 @@
 # Not necessarily the prettiest but easy to work with.
 
 # Petal reads this line.
-VERSION="1.0.0-rc4"
+VERSION="1.0.0-rc5"
 
 #(optional) what the webhook message should say.
 UPDATE_TITLE="The Great Migration"
