487c386ae9afd249cf45fb4bc058a8d602a7c9ee
Yaulendil <davarice@protonmail.com>
2020-09-04 23:05:56 -0400

    Rebase

--- version_info.sh
+++ version_info.sh
@@ -11,5 +11,4 @@ UPDATE_TITLE="Shove this Data into your Base"
 CHANGELOG=$(cat <<'EOF'
 * New MC submissions automatically check for alts.
 = Fixed various issues with AutoUnban and Minecraft DBs.
-EOF
 )
