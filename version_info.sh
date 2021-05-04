be1dbf9763652243f3b0b191f0bedcfd0d872b44
hdmifish <nullexistence180@gmail.com>
2021-05-04 17:35:42 -0500

    Update version_info.sh

--- version_info.sh
+++ version_info.sh
@@ -3,7 +3,7 @@
 # Not necessarily the prettiest but easy to work with.
 
 # Petal reads this line.
-VERSION="1.143"
+VERSION="1.1.4"
 
 #(optional) what the webhook message should say.
 UPDATE_TITLE="Shove this Data into your Base"
